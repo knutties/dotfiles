32a2913ddbed1a9b8ddc87a4f70854ba957eb708
Natarajan Kannan <knutties@gmail.com>
2012-04-25 22:56:42 +0530

    added symlinks to setup dot files on a new box

---
+++ dot-init/symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s $(realpath ../vimrc) ~/.vimrc
+ln -s $(realpath ../vim) ~/.vim
+ln -s $(realpath ../oh-my-zsh/zshrc) ~/.zshrc
+ln -s $(realpath ../oh-my-zsh/custom/plugins) ~/.oh-my-zsh/custom/
